00c6e05ec828da5f8485d19345611b26b4ddc07c
Pradipto Majumdar <pradiptom@nihilentanalytics.com>
2019-08-28 17:11:06 +0530

    1st commit

--- actions/hellotraining.py
+++ actions/hellotraining.py
@@ -4,7 +4,8 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
     
     def run(self,num1):
-    
+        requests
+        response
         response = requests.get("http://api.open-notify.org/iss-now.json")
         print(response.status_code)
         return(True)
