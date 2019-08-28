ba9c0807a56a636b72946aa6f8d332039434db7c
Pradipto Majumdar <pradiptom@nihilentanalytics.com>
2019-08-28 16:17:18 +0530

    first commit

--- actions/hellotraining.py
+++ actions/hellotraining.py
@@ -3,10 +3,11 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
     
-    def run(self, a,b,c):
-        thislist = []
-        thislist.append(a)
-        thislist.append(b)
-        thislist.append(c)
-        print(thislist)
+    def run(self,sub1,marks1,sub2,marks2,sub3,marks3):
+        thisdict = {}
+        thisdict[sub1]=marks1
+        thisdict[sub2]=marks2
+        thisdict[sub3]=marks3
+        for x in thisdict:
+            print(x+(str)thisdict[x])
         return(True)
