37a4e445f011d7922fc7163ce3baeff12b846186
Pradipto Majumdar <pradiptom@nihilentanalytics.com>
2019-08-28 15:40:42 +0530

    first commit

--- actions/hellotraining.py
+++ actions/hellotraining.py
@@ -3,7 +3,10 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
     
-    def run(self, a):
-        thislist = ["apple", "banana", "cherry"]
+    def run(self, a,b,c):
+        thislist = []
+        thislist.append(a)
+        thislist.append(b)
+        thislist.append(c)
         print(thislist)
         return(True)
